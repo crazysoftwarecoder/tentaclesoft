f64cc4f5d6cd7281942ec0f607e4b3754814ef39
Ashwanth Fernando <ashwanthfernando@gmail.com>
2025-03-01 00:34:34 +1100

    Added description

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
           <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
             <div className="flex flex-col justify-center space-y-4">
               <div className="space-y-2">
-                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                <h1 className="text-2xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none">
                   Turning Ideas Into World-Class Software
                 </h1>
-                <p className="max-w-[600px] text-muted-foreground md:text-xl">
+                <p className="max-w-[600px] text-muted-foreground md:text-lg">
                 We are the top 1% of engineers, building high-quality POCs and MVPs—then taking them all the way to production and beyond. With a proven track record of delivering scalable solutions for flagship tech companies, we provide end-to-end development and post-production support to ensure long-term success.
                 </p>
               </div>

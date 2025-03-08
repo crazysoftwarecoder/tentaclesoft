7d403aecf036a7d7d718c6d416b88bf2ae23fb87
Ashwanth Fernando <ashwanthfernando@gmail.com>
2025-03-08 23:00:57 +1100

    Added email functionality

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request) {
 
     const data = await resend.emails.send({
       from: 'Contact Form <onboarding@resend.dev>',
-      to: ['ashwanthfernando@gmail.com'],
+      to: ['senditmoiway@gmail.com'],
       subject: `Contact Form: ${subject}`,
       replyTo: email,
       text: `
